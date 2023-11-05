0d7a53368ad1bfc34839f172a12a7c5b3c470a1d
unknown <Albyhua@gmail.com>
2023-11-04 20:08:00 -0400

    updated schemas and models

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,45 +2,60 @@ const { Event, User, Comment } = require('../models');
 // auth 
 
 const resolvers = {
-    Query: {
+  Query: {
+
+  },
+
+  Mutation: {
+    // addUser: async (parent, args) => {
+    //     const user = await User.create(args);
+
+    //     return { user };
+    // },
+    // login: async ( parent, { email, password }) => {
+    //     const user = await User.findOne({ email });
+
+    //     if (!user) {
+    //         throw console.log("error");
+    //     }
+    //     // need auth 
+    // },
+    deleteUser: async (parent, { userID }, context) => {
+      if (context.user)
+        return User.findOneAndDelete({ _id: userID })
+
+      throw new Error("Something has gone wrong!");
+    },
+    addEvent: async (parent, { eventInput }, context) => {
+      if (context.user) { // needs testing
+        const event = await Event.create(eventInput);
+
+        await User.findByIdAndUpdate(context.user._id, {
+          $push: { events: event._id },
+        });
+
+        return event;
+      }
+
+      throw new Error("Something has gone wrong!");
 
     },
-
-    Mutation: {
-        // addUser: async (parent, args) => {
-        //     const user = await User.create(args);
-      
-        //     return { user };
-        // },
-        // login: async ( parent, { email, password }) => {
-        //     const user = await User.findOne({ email });
-
-        //     if (!user) {
-        //         throw console.log("error");
-        //     }
-        //     // need auth 
-        // },
-        deleteUser: async (parent, { userID }) => {
-            return User.findOneAndDelete({ _id: userID})
-        },
-        addEvent: async (parent, { Event }, context) => {
-            if (context.user) { // needs testing
-              const event = new Event({ title, description, date, time, location });
-      
-              await User.findByIdAndUpdate(context.user.id, {
-                $push: { events: event },
-              });
-      
-              return order;
-            }
-      
-            throw new Error("Something has gone wrong!");
-
-        },
-        updateEvent: async (parent, { Event }, context) => {
-
-        }
+    updateEvent: async (parent, { eventInput }, context) => {
+      if (context.user) {
+        const event = await Event.findOneAndUpdate({ _id: eventInput._id },
+          {
+            $set: eventInput
+          },
+          {
+            runValidators: true,
+            new: true,
+          }
+        )
+        return event;
+      }
+      throw new Error("Not logged in");
     }
+  }
 }
 
 module.exports = resolvers
