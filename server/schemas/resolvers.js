4ce73442b777add68582bffc52384449e9c56e75
AngelaFig <angelafigueroa596@gmail.com>
2023-11-05 16:32:13 -0500

    installed apollo client, added auth type to typedef

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,15 +3,11 @@ const { Event, User, Comment } = require('../models');
 
 const resolvers = {
   Query: {
-    getEventData: async (parent, { eventInput }) => {
-      return Event.findOne({_id: eventInput._id});
-    },
+    getEventData: async (parent, { eventInput }) =>
+      Event.findOne({ _id: eventInput._id }),
 
-    getUserEvents: async (parent, _, context) => {
-      return User.findOne({_id: context.user._id}).
-      populate('event')
-    },
-
+    getUserEvents: async (parent, _, context) =>
+      User.findOne({ _id: context.user._id }).populate('event'),
   },
 
   Mutation: {
@@ -63,76 +59,79 @@ const resolvers = {
         );
         return event;
       }
-      throw new Error("Not logged in");
+      throw new Error('Not logged in');
     },
 
     deleteEvent: async (parent, { eventInput }, context) => {
-      if (context.user._id === eventInput.hostID){
-        return Event.findOneAndDelete({_id: eventInput._id})
-      }else{
-        throw new Error("The user is not the host");
+      if (context.user._id === eventInput.hostID) {
+        return Event.findOneAndDelete({ _id: eventInput._id });
       }
+      throw new Error('The user is not the host');
     },
 
     addComment: async (parent, { eventInput, commentInput }, context) => {
-      if (context.user){
+      if (context.user) {
         return Event.findOneAndUpdate(
-          {_id: eventInput._id},
-          {$addToSet: {comment: {userId: contex.user._id, content: commentInput}}}
-        )
-      }else{
-        throw new Error ("Not logged in")
+          { _id: eventInput._id },
+          {
+            $addToSet: {
+              comment: { userId: contex.user._id, content: commentInput },
+            },
+          }
+        );
       }
+      throw new Error('Not logged in');
     },
 
     deleteComment: async (parent, { eventInput, commentInput }, context) => {
-      if (context.user === commentInput.userId){
+      if (context.user === commentInput.userId) {
         return Event.findOneAndUpdate(
-          {_id: eventInput._id},
-          {$pull: {comment: {_id: commentInput._id}}}
-        )
-      }else{
-        throw new Error ("Not your comment")
+          { _id: eventInput._id },
+          { $pull: { comment: { _id: commentInput._id } } }
+        );
       }
+      throw new Error('Not your comment');
     },
 
-    updateRSVP: async (parent, { eventInput, rsvpInput }, context) =>{
-      if (context.user){
+    updateRSVP: async (parent, { eventInput, rsvpInput }, context) => {
+      if (context.user) {
         return Event.findOneAndUpdate(
-          {_id: eventInput._id, "RSVP.userId": context.user._id},
-          {$set: {"RSVP.$.invite": rsvpInput}}
-          )
-      }else{
-        throw new Error ("Not logged in")
+          { _id: eventInput._id, 'RSVP.userId': context.user._id },
+          { $set: { 'RSVP.$.invite': rsvpInput } }
+        );
       }
+      throw new Error('Not logged in');
     },
 
-    addContribution: async (parent, { eventInput, contributionInput }, context) => {
-      if (context.user){
+    addContribution: async (
+      parent,
+      { eventInput, contributionInput },
+      context
+    ) => {
+      if (context.user) {
         return Event.findOneAndUpdate(
-          {_id: eventInput._id},
-          {$addToSet: {contribution: {item: contributionInput}}},
-        )
-      }else{
-        throw new Error ("Not logged in")
+          { _id: eventInput._id },
+          { $addToSet: { contribution: { item: contributionInput } } }
+        );
       }
+      throw new Error('Not logged in');
     },
-// Not sure if this needs to be its own function or if it should be part of updateEvent?
-    deleteContribution: async (parent, { eventInput, contributionInput }, context) => {
-      if (context.user){
+    // Not sure if this needs to be its own function or if it should be part of updateEvent?
+    deleteContribution: async (
+      parent,
+      { eventInput, contributionInput },
+      context
+    ) => {
+      if (context.user) {
         return Event.findOneAndUpdate(
-          {_id: eventInput._id},
+          { _id: eventInput._id },
           // Note: if there are two items with the same name, this may delete the wrong one. it would be more robust to have an id for each contribution. Maybe have a contributionSchema?
-          {$pull: {contribution: {item: contributionInput}}},
-        )
-      }else{
-        throw new Error ("Not logged in")
+          { $pull: { contribution: { item: contributionInput } } }
+        );
       }
+      throw new Error('Not logged in');
     },
-
-    
-
-  }
-}
+  },
+};
 
 module.exports = resolvers;
