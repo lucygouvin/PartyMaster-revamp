76d820f3f503644c0d6dfe8e418f34cd7f6be1d2
AngelaFig <angelafigueroa596@gmail.com>
2023-11-06 19:26:41 -0500

    added token decode auth to client side. Ignore changes made to app.jsx (I am sure Kojo made his own, coming soon)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import LandingPage from './components/LandingPage/LandingPage';
+
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache(),
+// });
 
 const App = () => {
   return (
