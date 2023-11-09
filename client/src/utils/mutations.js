b7bc3f1d463f37f9ddbeae83d25f2f7f0bd7caf1
AngelaFig <angelafigueroa596@gmail.com>
2023-11-08 20:01:52 -0500

    added data to Signup.jsx (email, name, password, handleformsubmit)

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql `
+mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+        email
+        password
+      }
+    }
+  }
+`
