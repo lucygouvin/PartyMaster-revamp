3ad3ac66c7c50330b9d3843074f2d9e8c0dfe3b8
Kojo Otchere <kojootchere@MacBook-Air-3.local>
2023-11-10 22:23:11 -0500

    Updated Landing Page, Added Confirm Password to Signup component and updated various styling sheets

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,36 +1,50 @@
 import React, { useState } from 'react';
 import '../../styles/LandingPage.css';
+import exampleImage from '../../assets/create-new-event.png';
+import exampleImage2 from '../../assets/checklist.png';
+import exampleImage3 from '../../assets/contributions.png';
 import { useQuery } from '@apollo/client';
 import { USERS } from '../../utils/queries';
 
 export function LandingPage(props) {
 
-  const {data, loading, error} = useQuery(USERS)
+  const { data, loading, error } = useQuery(USERS)
   const userData = data?.users || []
   // console log i sent you a pic of
   console.log(userData);
 
   if (loading) return <h2>Loading....</h2>
 
-    return (
+  return (
     <div className="landing-page">
       <main className="main-content">
-      {/* <div> {userData[0].name} </div> */}
-      <section className="features-overview">
-        <div className="feature">
-          <h2>Plan Events Effortlessly</h2>
-          <p>Create events, manage guest lists, and coordinate contributions all in one place.</p>
-        </div>
-        <div className="feature">
-          <h2>Stay Connected</h2>
-          <p>Communicate with guests, send invitations, and receive RSVPs in real-time.</p>
-        </div>
-        <div className="feature">
-          <h2>Track Contributions</h2>
-          <p>Manage contributions and expenses, and allow guests to chip in easily.</p>
-        </div>
-      </section>
-      {/* <section className="features-overview">
+        {/* <div> {userData[0].name} </div> */}
+        <section className="features-overview">
+          <h1 className="landing-page-header">What Can Party Master Do For You?</h1>
+          <div className="feature">
+            <img src={exampleImage} alt="Description" style={{ height: '200px', }} />
+            <div style={{ justifyContent: 'space-between', }}>
+              <h2>Plan Events Effortlessly</h2>
+              <p>Create events, manage guest lists, and coordinate contributions all in one place.</p>
+            </div>
+          </div>
+          <div className="feature">
+            <img src={exampleImage2} alt="Description" style={{ height: '200px', }} />
+            <div style={{ justifyContent: 'space-between', }}>
+              <h2>Stay Connected</h2>
+              <p>Communicate with guests, send invitations, and receive RSVPs in real-time.</p>
+            </div>
+          </div>
+          <div className="feature">
+            <img src={exampleImage3} alt="Description" style={{ height: '200px', }} />
+            <div style={{ justifyContent: 'space-between', }}>
+              <h2>Plan Events Effortlessly</h2>
+              <p>Create events, manage guest lists, and coordinate contributions all in one place.</p>
+            </div>
+          </div>
+          <a href="/signup" className="signup-button">Get Started</a>
+        </section>
+        {/* <section className="features-overview">
         <div className="feature">
           <h2>Sign Up</h2>
         </div>
