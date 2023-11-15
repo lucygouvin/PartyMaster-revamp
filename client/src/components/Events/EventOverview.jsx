d6248b1064a8a196392c9505ab20a81a249bdd19
lgouvinSchell <gouvin.lucy@gmail.com>
2023-11-14 22:39:48 -0500

    trying to fix RSVP dropdown behavior

--- client/src/components/Events/EventOverview.jsx
+++ client/src/components/Events/EventOverview.jsx
@@ -112,7 +112,10 @@ const EventOverview = () => {
     } catch (rsvpError) {
       console.error("Unable to update RSVP", rsvpError);
     }
-    setGuestRSVP(value);
+    if(typeof(value)===String){
+      setRSVP(value)
+
+    }
   };
   const inviteGuests = () => {
     const guestArray = inviteList.split(",");
@@ -171,6 +174,7 @@ const EventOverview = () => {
   useEffect(() => {
     if (hostID && rsvp) {
       setUserResponse(getUserRole(hostID, rsvp, user.data._id));
+      setGuestRSVP(userResponse.rsvp)
     }
   }, [hostID, rsvp]);
 
@@ -204,7 +208,6 @@ const EventOverview = () => {
     : 0,
     borderRadius:"5px",
   }
-
   return (
     <>
       <div className="flex-container"></div>
