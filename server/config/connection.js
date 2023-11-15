0978a345f4e42a92dc099dfcb8b79b9691e2b0d7
Kojo Otchere <kojootchere@MacBook-Air-3.local>
2023-11-14 20:42:13 -0500

    Revised emailBody constant in EventCreate to display webpage link

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  'mongodb+srv://Kojo:Kojo@partyplanner.isb2ed5.mongodb.net/'
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/partyplanner'
 );
 
 module.exports = mongoose.connection;
